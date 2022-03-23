abf09e28211eaacaff9be9572c13beb968beab27
lev-michael <michael.lev@hrsd.cz>
2022-03-23 19:05:36 +0100

    Initial commit

---
+++ src/main/java/cz/upce/fei/nnpia/nnpia_sem/NnpiaSemApplication.java
@@ -0,0 +1,13 @@
+package cz.upce.fei.nnpia.nnpia_sem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NnpiaSemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NnpiaSemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/upce/fei/nnpia/nnpia_sem/NnpiaSemApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.upce.fei.nnpia.nnpia_sem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NnpiaSemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
